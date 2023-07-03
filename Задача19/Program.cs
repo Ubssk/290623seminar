17c4c6a045335f3c5ade15ede373238d8102c8b6
Anna <99716@mail.ru>
2023-07-03 13:10:17 +0300

    Задачи

---
+++ Задача19/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите число: ");
+string stringNumber = Console.ReadLine();
+char number = ',';
+int index = stringNumber.IndexOf(number);
+if (stringNumber[0] == stringNumber[4] && stringNumber[1] == stringNumber[3])
+{
+    Console.WriteLine("Введенное число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Введенное число НЕ является палиндромом");
+}
---
+++ задача23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int start = 1;
+Console.WriteLine("Введите число: ");
+int end = Convert.ToInt32(Console.ReadLine());
+
+while (start <= end)
+{
+    Console.Write(Math.Pow(start,3) + " "); // start во степень
+    start++; // start = start + 1 - инкремент
+}
